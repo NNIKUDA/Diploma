0879861052103d4eb152daf45d7cca9115b52ec2
Nikita <777nikuda777@gmail,com>
2023-05-13 12:30:43 +0300

    register blog models in admin panel

--- webapp/blog/admin.py
+++ webapp/blog/admin.py
@@ -1,1 +1,19 @@
 from django.contrib import admin
+from .models import NewsTag, News, Comment
+
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(NewsTag)
+class NewsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Comment)
+class NewsAdmin(admin.ModelAdmin):
+    pass
+
+
