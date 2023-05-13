c60f58caf0a5eead7a2ca08137437526cae5b3e1
Nikita <777nikuda777@gmail,com>
2023-05-13 12:33:51 +0300

    Сustomized admin headers and title

--- webapp/webapp/urls.py
+++ webapp/webapp/urls.py
@@ -5,6 +5,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = 'WebShop administration'
+admin.site.index_title = 'WebShop App'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
