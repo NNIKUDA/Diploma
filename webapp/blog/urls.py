6be087aff242820af441f80f1082fb952617cddc
Nikita <777nikuda777@gmail,com>
2023-05-14 11:13:06 +0300

    Add urls for single page news and cats urls

--- webapp/blog/urls.py
+++ webapp/blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import blog
+from .views import blog, single
 
 
 urlpatterns = [
     path('', blog, name='blog'),
+    path('single/', single, name='single'),
+    path('<str:tag>', blog, name='tag')
 ]
 
